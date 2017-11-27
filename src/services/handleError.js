32e8ce5d0c3322d5dcb5fa0348a875c24c416f30
BIT Student <classroom@bgit.rs>
2017-11-27 14:45:01 +0100

    input polja

--- src/services/handleError.js
+++ src/services/handleError.js
@@ -4,27 +4,27 @@ export default class HandleErrorService {
     validateEmail(emailInput) {
         let regEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         let valid = regEmail.test(emailInput);
-        if(!valid){
+        if (!valid) {
             return "Invalid email";
         }
     }
 
     validateEmptyField(inputObject) {
-        for (const key in inputObject) {
-            if (inputObject[key] === "") {
-                return "All fields must be filled.";
+        //for (const key in inputObject) {
+        if (inputObject === "") {
+            return "All fields must be filled.";
 
-            }
         }
+        // }
     }
-    
+
     validateInputLength(input, numOfChar) {
         if (input.length < numOfChar) {
             return "The input is short.";
         }
     }
-    validateInputMatch(firstInput,secondInput){
-        if(firstInput !== secondInput){
+    validateInputMatch(firstInput, secondInput) {
+        if (firstInput !== secondInput) {
             return "Password does not match.";
         }
     }
