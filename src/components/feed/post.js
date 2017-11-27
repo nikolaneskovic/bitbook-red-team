6df73cc718a9c798b17ed696c90bd053ca6a495f
BIT Student <classroom@bgit.rs>
2017-11-27 12:34:13 +0100

    postst added in feed page

--- src/components/feed/post.js
+++ src/components/feed/post.js
@@ -6,15 +6,21 @@ class Post extends React.Component {
         super(props);
     }
     render() {
-        return (<div>
-
-            <p>{this.props.text}</p>
-        </div>);
+        return (
+            <div className="container">
+                <div className="row">
+                    <p className='col-12'>{this.props.text}</p>
+                    <p className="col-4">{this.props.userDisplayName}</p>
+                    <p className="col-4">{this.props.type}</p>
+                </div>
+            </div>);
     }
 }
 
 Post.propTypes = {
-    text: PropTypes.string
-    
+    text: PropTypes.string,
+    type: PropTypes.string,
+    userDisplayName: PropTypes.string
+
 };
 export default Post;
